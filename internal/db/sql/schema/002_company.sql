c5b90efd3ee70af66c3a56360732b250f355dc66
Philipp Selke <philipp.selke@gmail.com>
2026-08-17 21:56:03 +0200

    added the firms table and info over verifing these firms

--- internal/db/sql/schema/002_company.sql
+++ internal/db/sql/schema/002_company.sql
@@ -1,1 +1,12 @@
-
+-- +goose Up
+CREATE TABLE firms(
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  email TEXT NOT NULL,
+  ustId TEXT NOT NULL,
+  streetName TEXT NOT NULL,
+  plz TEXT NOT NULL
+);
+-- +goose Down
+DROP TABLE firms;
